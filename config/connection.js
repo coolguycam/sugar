1e7aa81297352054245be75be1560f7febbe26f2
Cameron Dimitroff <hawtrods@gmail.com>
2018-04-30 16:57:14 -0700

    heyya

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 const mysql = require("mysql");
 
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.eventNames.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   const connection = mysql.createConnection({
     port: 3306,
